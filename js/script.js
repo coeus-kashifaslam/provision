6c810c20223fb81a5be451076110889a7aa35702
coeus-kashifaslam <kashif.aslam@coeus-solutions.de>
2016-04-08 12:48:51 +0500

    Index page completed

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
 $(document).ready(function(){
-
+	$('.alert-dropdown .dropdown-toggle, .alert-dropdown .alert-close-control a').bind('click', function(e){
+		e.preventDefault();
+		$this1 = $(this);
+		$this1.closest('.dropdown').find('.dropdown-menu').toggleClass('alert-open');
+	});
 });
